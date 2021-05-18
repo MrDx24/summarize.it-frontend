4e1973380b78d799ce5f957fe8400287c96d208e
Deep Patadiya <deeppatadiya24@gmail.com>
2021-05-18 18:43:19 +0530

    updated files

--- src/app/my-summarizer/my-summarizer.component.ts
+++ src/app/my-summarizer/my-summarizer.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-var pdfMake = require('pdfmake/build/pdfmake.js');
-var pdfFonts = require('pdfmake/build/vfs_fonts.js');
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-my-summarizer',
